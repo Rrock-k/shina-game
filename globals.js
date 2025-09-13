43a434d1e0a3d0f259a13365c976cb1737361672
Rrock-k <32955276+Rrock-k@users.noreply.github.com>
2025-09-14 00:56:45 +0300

    chore: drop unused server and shared config

---
+++ globals.js
@@ -0,0 +1,27 @@
+export const globals = {
+  app: null,
+  world: null,
+  gridLayer: null,
+  roadsLayer: null,
+  lotsLayer: null,
+  zonesLayer: null,
+  labelsLayer: null,
+  intersectionsLayer: null,
+  decorLayer: null,
+  trafficLightsLayer: null,
+  borderLayer: null,
+  uiLayer: null,
+  lightingLayer: null,
+  car: null,
+  carPath: [],
+  carSegment: 0,
+  carProgress: 0,
+  avatar: null,
+  horizontalRoadYs: [],
+  verticalRoadXs: [],
+  hoverLabel: null,
+  carTrafficController: null,
+  buildingAvatars: new Map()
+};
+
+export default globals;
